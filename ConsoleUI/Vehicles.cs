f7404cde33e4f9bedbdedae96910c4db80a2cb6f
Zachary Whitley <zbwhitley@gmail.com>
2022-02-24 14:13:45 -0600

    implemented DriveA and DriveV

---
+++ ConsoleUI/Vehicles.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    public abstract class Vehicles
+    {
+        string Year;
+        string Make;
+        string Model;
+
+        public abstract void DriveAbstract();
+        public virtual void DriveVirtual() 
+        {
+            Console.WriteLine();
+        }
+    }
+}
